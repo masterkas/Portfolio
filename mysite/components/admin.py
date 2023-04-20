37a4e8dbc435b03a0fb6b42b9d4c828253a6a9dc
masterkas <mastervolkas@gmail.com>
2023-04-20 16:01:28 +0300

    20.04.2023

--- mysite/components/admin.py
+++ mysite/components/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class ComponentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cat', 'time_create', 'photo', 'is_publishes')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'description')
+    list_editable = ('is_publishes',)
+    list_filter = ('is_publishes', 'time_create')
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_comp')
+    list_display_links = ('id', 'type_comp')
+    search_fields = ('type_comp',)
+
+admin.site.register(Components, ComponentsAdmin)
+admin.site.register(Category, CategoryAdmin)
--- mysite/components/models.py
+++ mysite/components/models.py
@@ -5,14 +5,14 @@ from django.urls import reverse
 # Create your models here.
 
 class Components(models.Model):
-    name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    description = models.TextField(max_length=1000)
-    tel = models.CharField(max_length=10)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_upgrate = models.DateTimeField(auto_now=True)
-    is_publishes = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    name = models.CharField(max_length=100, verbose_name='название')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='фото')
+    description = models.TextField(max_length=1000, verbose_name='описание')
+    tel = models.CharField(max_length=10, verbose_name='телефон')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='время создания')
+    time_upgrate = models.DateTimeField(auto_now=True, verbose_name='последнее редактирование')
+    is_publishes = models.BooleanField(default=True, verbose_name='статус пупликации')
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
 
     def __str__(self):
         return self.name
@@ -20,12 +20,23 @@ class Components(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Компьютерные комплектующие'
+        verbose_name_plural = 'Компьютерные комплектующие'
+        ordering = ['-time_create', 'name']
 
 class Category(models.Model):
-    type_comp = models.CharField(max_length=50, db_index=True)
+    type_comp = models.CharField(max_length=50, db_index=True, verbose_name='тип компонента')
 
     def __str__(self):
         return self.type_comp
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+
+    class Meta:
+        verbose_name = 'Категория компонента'
+        verbose_name_plural = 'Категории компонентов'
+        ordering = ['id']
+
